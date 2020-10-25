154ef211b9e7e90ac31e42e8154b161089bc22f5
杜仁浩 <ak514250@126.com>
2020-10-25 13:43:59 +0800

    用户相关功能

--- src/main/java/com/weweibuy/bpms/BpmsApplication.java
+++ src/main/java/com/weweibuy/bpms/BpmsApplication.java
@@ -4,12 +4,14 @@ import org.flowable.spring.boot.FlowableSecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author durenhao
  * @date 2020/10/25 11:22
  **/
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, FlowableSecurityAutoConfiguration.class})
+@EnableFeignClients
 public class BpmsApplication {
 
     public static void main(String[] args) {
